e494a11252036a83386fcf9bb92cab562b6014bf
Aidan Maldonado <aidanmaldonado@aidans-air.lan>
2024-02-18 14:05:05 -0800

    Added Goals and beginning of class definition to mpl.py - More .py's to come?

--- mpl.py
+++ mpl.py
@@ -1,4 +1,31 @@
 ### Main Functions ###
+### Goals:
+# Linear Regression Model
+# Neural Network Model
+# Brain Inspired NNM
+# Gradient Descent (Stochastic too)
+# Principal Component Analysis and Dimension Reductionality
+# Regression & Classification Model
+# Support Vector Machine
+# Expecatation Classifciation
+# K Nearest Neighbors and Graph Work
+# Linear Multiclass Predictors
+# Naive Bayes
+# Singular Value Decomposition
+# Eigen Values and Eigen Vectors
+# Mean Square Error
+
+# to be continued
+
 import math
 
 # Data splitting
+def split():
+    return
+
+class LinearRegression(X_Train, Y_Train):
+    def __init__(self):
+        model = self.fit(X_Train, Y_Train)
+
+    def fit(self, X_Train, Y_Train):
+        return
