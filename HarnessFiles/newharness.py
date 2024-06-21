0cc3105f141391205bb046a1a2bd76b1071130e4
Aidan Maldonado <aidanmaldonado@aidans-air.lan>
2024-06-20 21:10:19 -0700

    Finalized Text Generation LM + Test Harness newharness.py

--- HarnessFiles/newharness.py
+++ HarnessFiles/newharness.py
@@ -7,20 +7,25 @@ test_data = "./A2-Data/1b_benchmark.test.tokens"
 sentence_starters = "./A2-Data/starter.txt"
 
 # Create an N-Gram Language Model
-bi = mplm.N_Gram(3, 1e-10)
+# Note: First parameter is 'n' grams, ie number of tokens in a gram. n-1 tokens for prefix context, 1 token for current suffix
+# Note: Second parameter is alpha-value for smoothing. Closer to 0 tends towards less Perplexity
+Trigram = mplm.N_Gram(3, 1e-10)
 
 # Train the N-Gram Language Model
+# Note: Linear Interpolation helps reduce perplexity, test with different lambda coefficients
 print("\nTraining . . .")
-bi.train(train_data)
+Trigram.train(train_data, interpolation=True, lambdas=[0.1, 0.3, 0.6])
 
 # Test the Perplexity of the N-Gram Model
 print("\nTesting . . .")
-print(f"Perplexity: {bi.test(test_data)}")
+print(f"Perplexity: {Trigram.test(test_data)}")
 
 # Create a Text Generator
-textGen = mplm.Generator(bi)
+TextGen = mplm.Generator(Trigram)
 
 # Generate an end to each sentene in the input file
+# Note: top_k means the sample size of most probable 'Next Words' most to randomly choose from
+# Note: temperature refers to 'how random' the selection is, choose close to 0 for Greedy, close to infinity for Uniform
 print("\nGenerating . . .")
-for filled in textGen.vectorfill(sentence_starters, top_k=10, temperature = 0.01):
+for filled in TextGen.vectorfill(sentence_starters, top_k=3, temperature = 0.05):
     print(filled)
